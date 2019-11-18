96c8f8fe9de7b3791d6372736a58c9794ef32ee3
Bobrov Oleg <olegich051@gmail.com>
2019-11-18 14:46:26 +0300

    Added renders. Parent should be set on instantiate on scene

--- static/PrefabCreator.cpp
+++ static/PrefabCreator.cpp
@@ -3,40 +3,52 @@
 //
 
 #include "PrefabCreator.h"
+#include "../core-components/renderers/CircleRenderer.h"
+#include "../core-components/renderers/LineRenderer.h"
 
-Point *PrefabCreator::createPoint(int idx) {
+Point *PrefabCreator::createPoint(int idx, Transform *parent) {
     GameObject *obj = new GameObject();
+    obj->transform->setParent(parent);
+    CircleRenderer *circleRenderer = obj->addComponent<CircleRenderer>();
+    circleRenderer->circle.setRadius(20);
+    circleRenderer->circle.setOrigin(20, 20);
     Point *point = obj->addComponent<Point>(idx);
     return point;
 }
 
-Line *PrefabCreator::createLine(int idx) {
+Line *PrefabCreator::createLine(int idx, Transform *parent) {
     GameObject *obj = new GameObject();
+    obj->transform->setParent(parent);
+    obj->addComponent<LineRenderer>();
     Line *line = obj->addComponent<Line>(idx);
     return line;
 }
 
-Train *PrefabCreator::createTrain(int idx) {
+Train *PrefabCreator::createTrain(int idx, Transform *parent) {
     GameObject *obj = new GameObject();
     Train *train = obj->addComponent<Train>(idx);
+    obj->transform->setParent(parent);
     return train;
 }
 
-Town *PrefabCreator::createTown(int idx) {
+Town *PrefabCreator::createTown(int idx, Transform *parent) {
     GameObject *obj = new GameObject();
     Town *town = obj->addComponent<Town>(idx);
+    obj->transform->setParent(parent);
     return town;
 }
 
-Market *PrefabCreator::createMarket(int idx) {
+Market *PrefabCreator::createMarket(int idx, Transform *parent) {
     GameObject *obj = new GameObject();
     Market *market = obj->addComponent<Market>(idx);
+    obj->transform->setParent(parent);
     return market;
 }
 
-Storage *PrefabCreator::createStorage(int idx) {
+Storage *PrefabCreator::createStorage(int idx, Transform *parent) {
     GameObject *obj = new GameObject();
     Storage *storage = obj->addComponent<Storage>(idx);
+    obj->transform->setParent(parent);
     return storage;
 }
 
