bce1f78d79b0d841129ebc7a5bcd3f634fb25421
Pavel <pawel00@mail.ru>
2019-11-18 14:07:06 +0300

    added classes for network

---
+++ Networking/Connection.hpp
@@ -0,0 +1,30 @@
+#ifndef CONNECTION_HPP
+#define CONNECTION_HPP
+
+#include <SFML/Network/TcpSocket.hpp>
+#include <SFML/Network/IpAddress.hpp>
+#include <cstdint>
+
+class Connection : protected sf::TcpSocket {
+    Connection() {}
+    Connection(const Connection &) = delete;
+    Connection(Connection &&) = delete;
+    Connection & operator=(const Connection &) = delete;
+    Connection & operator=(Connection &&) = delete;
+protected:
+    static Connection connection;
+    static const sf::IpAddress serverAddress;
+    static const uint16_t serverPort;
+    
+    // assuming that you first call login, than
+    // use instance as much as you want, than call
+    // logout
+    
+    friend class PacketQueue;
+public:
+    static void login();
+    static Connection & instance();
+    static void logout();
+};
+
+#endif // CONNECTION_HPP
