71cba8f6852afaf6c4d3de7407a7c84a57dc7f31
Bobrov Oleg <olegich051@gmail.com>
2019-11-21 01:10:42 +0300

    Visualizing graph iteratively

--- custom-components/GraphController.h
+++ custom-components/GraphController.h
@@ -8,10 +8,12 @@
 
 #include "../core/Component.h"
 #include "../json/json.hpp"
+#include "../json/GraphVisualizer.h"
 
 class GraphController : public Component {
 private:
     bool called = false;
+    GraphVisualizer graphVisualizer;
     nlohmann::json playerInfo;
 public:
     std::unordered_map<int, std::list<int>> graph;
@@ -19,7 +21,8 @@ public:
     void applyLayer0(const nlohmann::json& json);
     void applyLayer1(const nlohmann::json& json);
     void applyLayer10(const nlohmann::json& json);
-    void generateCoordinates();
+    void applyForceMethod();
+    void applyForceMethodIteration();
 
     void update() override;
 };
