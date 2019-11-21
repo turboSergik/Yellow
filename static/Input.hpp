b00033501bba5a3bb3eeb53fd8f45de694f57955
Bobrov Oleg <olegich051@gmail.com>
2019-11-21 15:59:10 +0300

    Held keys added

--- static/Input.hpp
+++ static/Input.hpp
@@ -12,48 +12,50 @@ class Input
     // sf::Keyboard::Return is last in int
     // enum, so it it + 1 is equal to numbers
     // of keys in enum
-    std::bitset<sf::Keyboard::KeyCount> pressedKeys;
-    std::bitset<sf::Keyboard::KeyCount> releasedKeys;
-    
-    std::bitset<sf::Mouse::ButtonCount> pressedMouseButtons;
-    std::bitset<sf::Mouse::ButtonCount> releasedMouseButtons;
-    
-    std::array<sf::Event::MouseButtonEvent, 
+    static std::bitset<sf::Keyboard::KeyCount> pressedKeys;
+    static std::bitset<sf::Keyboard::KeyCount> heldKeys;
+    static std::bitset<sf::Keyboard::KeyCount> releasedKeys;
+
+    static std::bitset<sf::Mouse::ButtonCount> pressedMouseButtons;
+    static std::bitset<sf::Mouse::ButtonCount> releasedMouseButtons;
+
+    static std::array<sf::Event::MouseButtonEvent,
                sf::Mouse::ButtonCount> pressedMouseEvents;
-    
-    std::array<sf::Event::MouseButtonEvent,
+
+    static std::array<sf::Event::MouseButtonEvent,
                sf::Mouse::ButtonCount> releasedMouseEvents;
-    
-    sf::Event::MouseWheelScrollEvent wheelScrollEvent;
-    bool wheelScrolled;
-    
-    
-    Input() = default;
-    Input(const Input &) = delete;
-    Input(Input &&) = delete;
-    Input & operator=(const Input &) = delete;
-    Input & operator=(Input &&) = delete;
+
+    static sf::Event::MouseWheelScrollEvent wheelScrollEvent;
+    static bool wheelScrolled;
+
+//    Input() = default;
+//    Input(const Input &) = delete;
+//    Input(Input &&) = delete;
+//    Input & operator=(const Input &) = delete;
+//    Input & operator=(Input &&) = delete;
 public:
-    static Input & instance();
-    
-    void addKeyPressed(sf::Event::KeyEvent keyEvent);
-    void addKeyReleased(sf::Event::KeyEvent keyEvent);    
-    bool getKeyPressed(sf::Keyboard::Key key);
-    bool getKeyReleased(sf::Keyboard::Key key);
-    
-    void addMouseButtonPressed(sf::Event::MouseButtonEvent buttonEvent);
-    void addMouseButtonReleased(sf::Event::MouseButtonEvent buttonEvent);
-    bool getMouseButtonPressed(sf::Mouse::Button button);
-    bool getMouseButtonReleased(sf::Mouse::Button button);
-    sf::Event::MouseButtonEvent getMBPressedEvent(sf::Mouse::Button button);
-    sf::Event::MouseButtonEvent getMBReleasedEvent(sf::Mouse::Button button);
-    
-    void addWheelScroll(sf::Event::MouseWheelScrollEvent mouseScrollEvent);
-    sf::Event::MouseWheelScrollEvent getWheelScrollEvent();
-    bool getWheelScrolled();
-    
-    void reset();
-    ~Input() = default;
+//    static Input & instance();
+//    ~Input() = default;
+
+    static void addKeyPressed(sf::Event::KeyEvent keyEvent);
+    static void addKeyReleased(sf::Event::KeyEvent keyEvent);
+
+    static bool isKeyDown(sf::Keyboard::Key key);
+    static bool isKey(sf::Keyboard::Key key);
+    static bool isKeyUp(sf::Keyboard::Key key);
+
+    static void addMouseButtonPressed(sf::Event::MouseButtonEvent buttonEvent);
+    static void addMouseButtonReleased(sf::Event::MouseButtonEvent buttonEvent);
+    static bool getMouseButtonPressed(sf::Mouse::Button button);
+    static bool getMouseButtonReleased(sf::Mouse::Button button);
+    static sf::Event::MouseButtonEvent getMBPressedEvent(sf::Mouse::Button button);
+    static sf::Event::MouseButtonEvent getMBReleasedEvent(sf::Mouse::Button button);
+
+    static void addWheelScroll(sf::Event::MouseWheelScrollEvent mouseScrollEvent);
+    static sf::Event::MouseWheelScrollEvent getWheelScrollEvent();
+    static bool getWheelScrolled();
+
+    static void reset();
 };
 
 #endif // INPUT_HPP
