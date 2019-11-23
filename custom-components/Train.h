45dca3059b844f2bb819244d389f36cd54d5a78d
Bobrov Oleg <olegich051@gmail.com>
2019-11-23 12:29:42 +0300

    Safe init

--- custom-components/Train.h
+++ custom-components/Train.h
@@ -13,19 +13,19 @@ public:
     explicit Train(int idx);
 
     int cooldown = 0;
-    //int fuel = 0;
-    //int fuel_capacity = 0;
-    //int fuel_consumption = 1;
+    int fuel = 0;
+    int fuel_capacity = 0;
+    int fuel_consumption = 1;
     int goods = 0;
     int goods_capacity = 40;
-    //TODO: ask what this field store
-    //"goods_type": null, WTF?
     int level = 1;
     int next_level_price = 40;
-    //"player_idx": "a33dc107-04ab-4039-9578-1dccd00867d1",
     int position = 0;
     int speed = 0;
+    std::string player_idx;
     Line *line = nullptr;
+    //TODO: ask what this field store
+    //"goods_type": null, WTF?
 
     void applyLayer1(const nlohmann::json &json) override;
     void update() override ;
