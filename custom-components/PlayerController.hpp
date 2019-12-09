4290b48cacbb8ec15e8432ac218152be470f0c73
Bobrov Oleg <olegich051@gmail.com>
2019-12-10 02:48:18 +0300

    PlayerController for gameLogic

---
+++ custom-components/PlayerController.hpp
@@ -0,0 +1,29 @@
+//
+// Created by Олег Бобров on 10.12.2019.
+//
+
+#ifndef WG_PLAYERCONTROLLER_HPP
+#define WG_PLAYERCONTROLLER_HPP
+
+
+#include "../core/Component.h"
+#include "Point.h"
+
+class PlayerController : public Component {
+private:
+    float timeFromLastTurn = 0.5f;
+    float waitingTime = 0.5f;//time to wait (show animations)
+public:
+    Point * playerPoint = nullptr;
+    Town * playerTown = nullptr;
+    std::vector<Train*> playerTrains;
+    std::vector<Market*> markets;
+    std::vector<Storage*> storages;
+    bool isMapUpdated = false;
+
+    void update();
+    void strategyIteration();
+};
+
+
+#endif //WG_PLAYERCONTROLLER_HPP
