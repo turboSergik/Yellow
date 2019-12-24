2edcfe29d836c3e4a524fb54d7ebd5bece2288d3
turboSergik <serg.piskun111@mail.ru>
2019-12-24 12:15:29 +0300

    Bag with crash trains

--- custom-components/GraphController.cpp
+++ custom-components/GraphController.cpp
@@ -177,7 +177,7 @@ void GraphController::start() {
 }
 
 void GraphController::update() {
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < 20; i++) {
         GraphController::applyForceMethodIteration();
     }
 }
