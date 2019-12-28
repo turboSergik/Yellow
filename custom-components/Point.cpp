05b931fa6cd84f778564b5d946ade91b11a99415
Bobrov Oleg <olegich051@gmail.com>
2019-12-28 19:06:36 +0300

    friction force

--- custom-components/Point.cpp
+++ custom-components/Point.cpp
@@ -4,6 +4,7 @@
 
 #include "Point.h"
 #include "../static/Database.h"
+#include "../core/GameObject.h"
 
 void Point::applyLayer10(const nlohmann::json &json) {
     transform->setLocalPosition({json.value("x", 0.f),
@@ -20,4 +21,17 @@ void Point::applyLayer0(const nlohmann::json &json) {
 
 Point::Point(int idx) : Behaviour(idx) {
 
+}
+
+void Point::start() {
+    rigidBody = gameObject->getComponent<RigidBody>();
+}
+
+void Point::update() {
+    float sqrMagnitude = rigidBody->velocity.x * rigidBody->velocity.x +
+            rigidBody->velocity.y*rigidBody->velocity.y;
+    float magnitude = sqrtf(sqrMagnitude);
+    sf::Vector2f direction = -rigidBody->velocity / magnitude;
+    rigidBody->addForce(direction * (frictionK * sqrMagnitude + dampK * magnitude));
+    //rigidBody->addForce(direction * (frictionK * sqrMagnitude));
 }
--- custom-components/Point.h
+++ custom-components/Point.h
@@ -6,17 +6,25 @@
 #define WG_POINT_H
 
 #include "Behaviour.h"
+#include "../core-components/RigidBody.hpp"
 
 class Post;
 
 class Point : public Behaviour {
+private:
+    float frictionK = 0.05f;
+    float dampK = 0.1f;
 public:
-    Post* post = nullptr;
+    RigidBody * rigidBody = nullptr;
+    Post * post = nullptr;
     std::list<std::pair<Point*, Line*>> adjacent;
 
     explicit Point(int idx);
     void applyLayer10(const nlohmann::json &json) override;
     void applyLayer0(const nlohmann::json &json) override;
+
+    void start();
+    void update();
 };
 
 
