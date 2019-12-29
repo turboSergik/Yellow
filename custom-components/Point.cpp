0e38341244ede3940d50c7961283370f5f33a874
Bobrov Oleg <olegich051@gmail.com>
2019-12-30 02:45:08 +0300

    refactor

--- custom-components/Point.cpp
+++ custom-components/Point.cpp
@@ -10,15 +10,15 @@
 #include "../linalg/Vector2.hpp"
 
 void Point::applyLayer10(const nlohmann::json &json) {
-    transform->setLocalPosition({json.value("x", 0.f),
-                                 json.value("y", 0.f)});
+    this->transform->setLocalPosition({
+        json.value("x", 0.f),
+        json.value("y", 0.f) });
 }
 
 void Point::applyLayer0(const nlohmann::json &json) {
-    adjacent.clear();
+    this->adjacent.clear();
     if (json.contains("post_idx")) {
-        auto & post_idx = json["post_idx"];
-        Point::post = post_idx == nullptr ? nullptr : Database::posts[post_idx];
+        this->post = json["post_idx"].is_null() ? nullptr : Database::posts[json["post_idx"]];
     }
 }
 
@@ -27,12 +27,12 @@ Point::Point(int idx) : Behaviour(idx) {
 }
 
 void Point::start() {
-    rigidBody = gameObject->getComponent<RigidBody>();
+    this->rigidBody = this->gameObject->getComponent<RigidBody>();
 }
 
 void Point::update() {
     Vector2 direction = -rigidBody->velocity;
-    rigidBody->addForce(direction.normalized() * (ForceMethodConfig::frictionK
+    this->rigidBody->addForce(direction.normalized() * (ForceMethodConfig::frictionK
     * direction.sqrMagnitude() + ForceMethodConfig::dampK * sqrtf(direction.magnitude())));
     //rigidBody->addForce(direction.normalized() *(frictionK * direction.sqrMagnitude()));
 }
