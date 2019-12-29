93df3fe2e39a1225852f2fb9b247e59cbebe5986
Bobrov Oleg <olegich051@gmail.com>
2019-12-29 16:30:15 +0300

    default resolution changed

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@
 int main() {
     srand(time(nullptr));
 
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Graph");
+    sf::RenderWindow window(sf::VideoMode(1600, 900), "Graph");
     window.setFramerateLimit(60);
 
     GameObject * root = Prefabs::graphRoot()->gameObject->instantiate();
