b187607cc41f128cabb70f7c01081d565fde7378
Bobrov Oleg <olegich051@gmail.com>
2020-01-12 22:05:56 +0300

    scroll to cursor

--- custom-components/CameraController.cpp
+++ custom-components/CameraController.cpp
@@ -16,34 +16,39 @@ void CameraController::start() {
 
 void CameraController::update() {
     Camera * mainCamera = Camera::mainCamera;
-    Vector2 pos = this->transform->getLocalPosition();
+    Vector2 pos = this->transform->getPosition();
     if (Input::isKey(up)) {
         pos += {0.f, mainCamera->getWidth()*moveFactor*Time::deltaTime};
-        this->transform->setLocalPosition(pos);
     }
     if (Input::isKey(left)) {
         pos -= {mainCamera->getWidth()*moveFactor*Time::deltaTime,0.f };
-        this->transform->setLocalPosition(pos);
     }
     if (Input::isKey(down)) {
         pos -= {0.f, mainCamera->getWidth()*moveFactor*Time::deltaTime};
-        this->transform->setLocalPosition(pos);
     }
     if (Input::isKey(right)) {
         pos += {mainCamera->getWidth()*moveFactor*Time::deltaTime,0.f };
-        this->transform->setLocalPosition(pos);
     }
     if (Input::wheelScrolled) {
-
         this->targetWidth -=
                 this->targetWidth *
                 this->scrollFactor *
                 Input::wheelScrollEvent.delta;
         this->targetWidth = Mathf::clamp(this->targetWidth, this->minSize, this->maxSize);
+        this->cursorScreenPosition = Input::mousePosition;
+        this->targetTranslation = mainCamera->screenToWorldPoint(this->cursorScreenPosition) - pos;
     }
     float width = mainCamera->getWidth();
-    if (fabs(targetWidth - width) > 1e-5) {
+    if (fabs(this->targetWidth - width) > 1e-2) {
         mainCamera->setWidth(
                 Mathf::lerp(width, this->targetWidth, this->scrollSpeed*Time::deltaTime));
+        Vector2 currentTranslation = mainCamera->screenToWorldPoint(this->cursorScreenPosition) - pos;
+        pos += this->targetTranslation - currentTranslation;
+        this->targetTranslation = currentTranslation;
+//        Vector2 a = this->targetTranslation;
+//        std::cout << a.x << " " << a.y <<  " ";
+//        Vector2 b = currentTranslation;
+//        std::cout << b.x << " " << b.y << std::endl;
     }
+    this->transform->setPosition(pos);
 }
